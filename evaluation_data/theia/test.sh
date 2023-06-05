4410244f913e98944d072620f974214ee3f8f65c
Chieh En Hsieh <chiehen.hsieh@tum.de>
2023-06-05 14:25:52 +0200

    add theia

---
+++ evaluation_data/theia/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get the number of test executions from the first command line argument
+EXECUTIONS=$1
+
+# Activate noise
+noise-tool activate
+
+mkdir -p /test-results
+for i in $(seq 1 $EXECUTIONS); do
+    echo "[start] Test suite run $i"
+    PLAYWRIGHT_JUNIT_OUTPUT_NAME=test-results/results.xml yarn ui-tests --reporter=junit
+    mv "/shiki/test-results/results.xml" "/test-results/results_$i.xml"
+    kill -9 $(lsof -t -i:3000)  # kill the server
+    echo "[finished] Test suite run $i"
+done
+
+# Deactivate noise
+noise-tool deactivate
+
+# Get test outputs
+aggregate-test-results parse-junit-xml /test-results
+# Create artifacts
+aggregate-test-results create-artifacts /test-results /experiment-artifacts --aggregation-format junit-xml
