68b4d0f9581f18397ba3a2768c027aeef4b7beb9
Chieh En Hsieh <chiehen.hsieh@tum.de>
2023-07-02 01:05:42 +0200

    100 times or 10 hrs for all projects

--- evaluation_data/storybook/test.sh
+++ evaluation_data/storybook/test.sh
@@ -14,11 +14,28 @@ export CI=1
 noise-tool activate
 
 mkdir -p /test-results
+max_time=36000 # 10 hrs
+elapsed_time=0
 for i in $(seq 1 $EXECUTIONS); do
+    test_start=$(date +%s)
     echo "[start] Test suite run $i"
     yarn task --task e2e-tests --template=react-vite/default-ts --start-from=auto
     mv "/current-test-results/results.xml" "/test-results/results_$i.xml"
     echo "[finished] Test suite run $i"
+
+    test_end=$(date +%s)
+    # check if execution time will exceeds
+    test_time=$((test_end - test_start))
+    echo "one iteration time: $test_time"
+    elapsed_time=$((elapsed_time + test_time))
+    avg_time=$((elapsed_time / i))
+    echo "Avg time per iteration so far: $avg_time seconds."
+    projected_end_time=$((elapsed_time + avg_time))
+    if [ $projected_end_time -ge $max_time ]; then
+        echo "Projected execution time for the next iteration will exceed the time limit."
+        echo "Number of executions done: $i"
+        break
+    fi
 done
 
 # Deactivate noise
--- evaluation_data/theia/test.sh
+++ evaluation_data/theia/test.sh
@@ -21,11 +21,30 @@ yarn --cwd examples/playwright theia:start &
 noise-tool activate
 
 mkdir -p /test-results
+max_time=36000 # 10 hrs
+elapsed_time=0
 for i in $(seq 1 $EXECUTIONS); do
+    test_start=$(date +%s)
+    
     echo "[start] Test suite run $i"
     yarn --cwd examples/playwright ui-tests
     mv "/current-test-results/results.xml" "/test-results/results_$i.xml"
     echo "[finished] Test suite run $i"
+
+    test_end=$(date +%s)
+
+    # check if execution time will exceeds
+    test_time=$((test_end - test_start))
+    echo "one iteration time: $test_time"
+    elapsed_time=$((elapsed_time + test_time))
+    avg_time=$((elapsed_time / i))
+    echo "Avg time per iteration so far: $avg_time seconds."
+    projected_end_time=$((elapsed_time + avg_time))
+    if [ $projected_end_time -ge $max_time ]; then
+        echo "Projected execution time for the next iteration will exceed the time limit."
+        echo "Number of executions done: $i"
+        break
+    fi
 done
 
 # Deactivate noise
